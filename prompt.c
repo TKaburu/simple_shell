29bb155fefcf0ab9f219d6b59d92d3a4aed3c5ad
TKaburu <traceymwendwa@gmail.com>
2023-09-15 09:33:03 -0700

    Handles commands with arguments

---
+++ prompt.c
@@ -0,0 +1,120 @@
+#include "main.h"
+
+/**
+ * prompt - prints prompt to user
+ *
+ */
+
+void prompt(void)
+{
+	if (isatty(STDIN_FILENO))
+	{
+		write(STDOUT_FILENO, "$ ", 2);
+		fflush(stdout);
+	}
+}
+
+
+/**
+ * read_cmd - This function reads the users input
+ * @comnd: This is the input of the user
+ *
+ */
+
+void read_cmd(char *comnd)
+{
+	ssize_t input;
+	int k = 0;
+
+	input = read(STDIN_FILENO, comnd, MAX_PROMPT);
+	if (input == -1)
+	{
+		exit(errno);
+	}
+	while (comnd[k])
+	{/* remove '\n' */
+		if (comnd[k] == '\n')
+			comnd[k] = '\0';
+		k++;
+	}
+	if (input == '\0' || input == EOF)
+		exit(EXIT_SUCCESS);
+
+}
+
+/**
+ * split - This function splits string according to delimeter
+ * @comnd: The string
+ * @store: Adress of individual token
+ *
+ * Return: individual tokens
+ */
+
+
+char **split(char *comnd, char **store)
+{
+	char *point;
+	char *delim = " \n\t";
+	int k = 0;
+
+	point = strtok(comnd, delim);
+	while (point)
+	{
+		store[k++] = point;
+		point = strtok(NULL, delim);
+	}
+	store[k] = NULL;
+	return (store);
+}
+
+
+/**
+ * exec_cmnd - This function executes commands
+ * @comnd: uer command to be executed
+ *
+ */
+
+void exec_cmnd(char *comnd)
+{
+	pid_t baby;
+	char *arg[] = { NULL, NULL };
+	int status;
+
+	baby = fork();
+	if (baby == -1)
+	{
+		perror("Error: Failed to create baby");
+		exit(errno);
+	}
+	else if (baby == 0)
+	{
+		split(comnd, arg);
+		arg[0] = comnd;
+		if (execve(arg[0], arg, environ) == -1)
+		{
+			perror("./hsh");
+			exit(errno);
+		}
+	}
+	else
+		wait(&status);
+}
+/**
+ * main - Entry point of the shell program
+ *
+ * Return: (0) on succes
+ */
+
+int main(void)
+{
+	char comnd[MAX_PROMPT];
+
+	for (;;)
+	{
+		prompt();
+		read_cmd(comnd);
+		exec_cmnd(comnd);
+
+	}
+	return (0);
+}
