30013f319d938a7e0a263f52a1e6922624c817b3
TKaburu <traceymwendwa@gmail.com>
2023-09-19 23:02:56 -0700

    Handles comments

---
+++ handlers.c
@@ -0,0 +1,22 @@
+#include "main.h"
+
+
+/**
+ * handle_comment - This function check if string has
+ * comment (#) and voids it
+ * @comnd: The string to be checked
+ *
+ */
+
+void handle_comment(char *comnd)
+{
+        int k = 0;
+
+        while (comnd[k])
+        {
+                if (comnd[k] == '#')
+                        comnd[k] = '\0';
+                k++;
+        }
+}
+
